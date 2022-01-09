144059446b14cb091b23cd26b62f680107e2c95a
Fabian Schwarz <fabian12943@gmail.com>
2022-01-09 01:28:40 +0100

    Add functionality to create new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,17 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    article_params = params.require(:article).permit(:title, :description)
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
 end
