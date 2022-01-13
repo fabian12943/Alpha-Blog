6cf3e1a87adfa75abc300d2f62393d25f3acb9b2
Fabian Schwarz <fabian12943@gmail.com>
2022-01-13 03:23:17 +0100

    Generate random fake data for article cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,20 @@
-class PagesController < ApplicationController  
+require 'faker'
+
+class PagesController < ApplicationController
+
+  def home
+    cardsToCreate = 8
+    @names = []
+    @dates = []
+    @titles = []
+    @descriptions = []
+
+    cardsToCreate.times do |i|
+      @names << Faker::Name.name
+      @dates << rand(2.years).seconds.ago.strftime('%a., %d %b %Y, %H:%M:%S')
+      @titles << Faker::Lorem.sentence(word_count: 6)
+      @descriptions << Faker::Lorem.paragraph(sentence_count: 6) + " [...]"
+    end
+  end
+
 end
