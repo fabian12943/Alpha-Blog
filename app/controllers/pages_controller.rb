86320be673baa104aba5929711cfffc927786a98
Fabian Schwarz <fabian12943@gmail.com>
2022-01-15 02:02:09 +0100

    Rename variable to match snake case (SonarQube)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,13 @@ require 'faker'
 class PagesController < ApplicationController
 
   def home
-    cardsToCreate = 8
+    amount_of_cards = 8
     @names = []
     @dates = []
     @titles = []
     @descriptions = []
 
-    cardsToCreate.times do |i|
+    amount_of_cards.times do |i|
       @names << Faker::Name.name
       @dates << rand(2.years).seconds.ago.strftime('%a., %d %b %Y, %H:%M:%S')
       @titles << Faker::Lorem.sentence(word_count: 6)
