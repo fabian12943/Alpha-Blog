af8003f52b15cb4e6d82247c74fb2dc708d9dca3
Fabian Schwarz <fabian12943@gmail.com>
2022-01-18 01:04:38 +0100

    Increase maximum length for article title and description

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 120 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 10000 }
 end
