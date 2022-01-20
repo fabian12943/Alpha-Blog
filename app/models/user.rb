43cea12b111270ebf743fecae2dd3ad208888cce
Fabian Schwarz <fabian12943@gmail.com>
2022-01-20 19:48:36 +0100

    Add secure password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :articles, dependent: :destroy
 
+  has_secure_password
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 25 }
   validates :first_name, presence: true, length: { minimum: 2, maximum: 25 }
   validates :last_name, presence: true, length: { minimum: 2, maximum: 25 }
