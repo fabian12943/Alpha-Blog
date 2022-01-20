c9268ef9add7f28041f309c3ca3a067b6db0fe26
Fabian Schwarz <fabian12943@gmail.com>
2022-01-20 01:42:22 +0100

    Downcase email, first- and lastname of user before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { minimum: 2, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, email: true, length: { maximum: 100 }
 
+  before_save { self.email.downcase! }
+  before_save { self.first_name.downcase! }
+  before_save { self.last_name.downcase! }
+
   def fullname
     "#{first_name} #{last_name}"
   end
