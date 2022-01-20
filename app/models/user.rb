e73a8f6d3e35971f3afaa14632f166dd41f0e44c
Fabian Schwarz <fabian12943@gmail.com>
2022-01-20 01:03:41 +0100

    Add user validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :articles, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 25 }
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, email: true, length: { maximum: 100 }
+
 end
