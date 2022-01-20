25268debfbbaf07ca57013b6f18ddb5db026c680
Fabian Schwarz <fabian12943@gmail.com>
2022-01-21 00:31:50 +0100

    Replace placeholder articles on index view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,8 @@
-require 'faker'
-
 class PagesController < ApplicationController
+  @@AMOUNT_OF_ARTICLES = 6
 
   def home
-    amount_of_cards = 8
-    @names = []
-    @dates = []
-    @titles = []
-    @descriptions = []
-
-    amount_of_cards.times do |i|
-      @names << Faker::Name.name
-      @dates << rand(2.years).seconds.ago.strftime('%a., %d %b %Y, %H:%M:%S')
-      @titles << Faker::Lorem.sentence(word_count: 6)
-      @descriptions << Faker::Lorem.paragraph(sentence_count: 15)
-    end
+    @articles = Article.order(created_at: :desc).limit(@@AMOUNT_OF_ARTICLES)
   end
 
 end
