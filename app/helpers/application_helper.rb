9e889dbc2f93afb1cd865e88ca5f8cba8bf6fe45
Fabian Schwarz <fabian12943@gmail.com>
2022-01-23 19:43:07 +0100

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
     end
   end
 
+  def gravatar_url_for(user, options = { size: 80 })
+    email = user.email
+    hash = Digest::MD5.hexdigest(email)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=robohash"
+    #image_tag(gravatar_url, alt: user.fullname, class: "gravatar")
+  end
+
 end
