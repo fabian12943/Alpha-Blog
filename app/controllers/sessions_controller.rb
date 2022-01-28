4886fc5a11dca7d80e329873899f219a92c7e8d5
Fabian Schwarz <fabian12943@gmail.com>
2022-01-28 17:24:51 +0100

    Add functionality to create and destroy user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,29 @@
 class SessionsController < ApplicationController
 
   def create
+    user = User.find_by(email: login_params[:email].downcase)
+    if user && user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+      redirect_to request.referer || root_path, :notice => "Welcome back, #{user.fullname}!"
+    else
+      redirect_to request.referer || root_path, :alert => "Sorry, your email or password is incorrect."
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to request.referer || root_path, :notice => "You have successfully logged out."
   end
+
+  private 
+
+  def set_article
+    @user = User.find(params[:id])
+  end
+
+  def login_params
+    params.require(:session).permit(:email, :password)
+  end
+  
 
 end
