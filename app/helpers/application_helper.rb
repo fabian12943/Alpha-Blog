6ce64d02ef8fce8f9361af81c3a2c1f3456f34f7
Fabian Schwarz <fabian12943@gmail.com>
2022-01-30 00:02:29 +0100

    Add markdown support for article description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,23 @@ module ApplicationHelper
     end
   end
 
+  class CodeRayify < Redcarpet::Render::HTML
+   def block_code(code, language)
+      language = :text if language == nil || !CodeRay::Scanners.list.include?(language.to_sym)
+      CodeRay.scan(code, language).div
+   end
+  end
+ 
+  def markdown(text)
+    coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+    options = {
+    fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
+
 end
