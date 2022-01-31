4574ca3e168f68777f343d04f86b13fa84aa8b1c
Fabian Schwarz <fabian12943@gmail.com>
2022-01-31 01:39:03 +0100

    Give admin rights to edit or delete any article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :set_prev_page, only: [:edit]
   before_action :require_login, except: [:index, :show]
-  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user_or_admin, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
@@ -49,8 +49,8 @@ class ArticlesController < ApplicationController
     @prev_page = request.referer || articles_path
   end
 
-  def require_same_user
-    if current_user != @article.user
+  def require_same_user_or_admin
+    if current_user != @article.user && !current_user.admin?
       redirect_to @article, alert: "You are not authorized to edit or delete this article."
     end
   end
