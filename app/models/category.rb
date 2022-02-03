dd12c890edbd340b7dc6bcb72ebf4387af691d94
Fabian Schwarz <fabian12943@gmail.com>
2022-02-03 01:43:30 +0100

    Create association between articles and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   PAGE_LIMIT = 12
+  has_and_belongs_to_many :articles
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 15 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_164559) do
+ActiveRecord::Schema.define(version: 2022_02_03_003555) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2022_01_31_164559) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
   end
 
   create_table "categories", force: :cascade do |t|
