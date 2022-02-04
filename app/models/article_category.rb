1dc8fc4aa722d3cb6d761f976ef5ed0e2aedbfde
Fabian Schwarz <fabian12943@gmail.com>
2022-02-04 03:09:18 +0100

    Link most popular categories in articles dropdown in navigation bar

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,4 +1,12 @@
 class ArticleCategory < ApplicationRecord
   belongs_to :article
   belongs_to :category
+
+  def self.most_used_categories(amount_of_categories)
+    ArticleCategory.select('category_id, count(category_id) as article_count')
+                   .group('category_id')
+                   .order('article_count DESC')
+                   .limit(amount_of_categories)
+                   .map { |article_category| article_category.category }
+  end
 end
